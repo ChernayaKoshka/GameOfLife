7d2cf2a071ba5a69fc862fd3869e47787cfd22e5
Christopher A <chris.altig@gmail.com>
2016-06-29 20:22:45 -0700

    covnerted options to static globals

--- GameOfLife/GameOfLife/GlobalOptions.h
+++ GameOfLife/GameOfLife/GlobalOptions.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#define WINDOW_WIDTH 1280
-#define WINDOW_HEIGHT 720
+static int gWindowWidth = 1280;
+static int gWindowHeight = 720;
 
-static double DENSITY = 0.35;
-static int BORDERS = 0;
-static int CELL_SIZE = 8;
-static int SIMULATION_MODE = 0;
+static double gDensity = 0.35;
+static int gBoundary = 0;
+static int gCellSize = 8;
+static int gSimulationMode = 0;
 
-static float STEPS_PER_SECOND = 0.05f;
+static float gStepsPerSecond = 0.05f;
